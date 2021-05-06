8afc44e1fea5786d87f111562453e2b704a1a655
Nikitaasdrf <ryzhov.nickit@yandex.ru>
2021-05-06 13:01:09 +0300

    first commit

--- politex/settings.py
+++ politex/settings.py
@@ -120,9 +120,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'mainApp/media')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
